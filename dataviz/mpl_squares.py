a73bf5aa7a01d3e5df6b6256f0016a6fda25baaf
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-03-26 21:30:34 +0200

    started playing with tutorial again, ignored .idea file

--- dataviz/mpl_squares.py
+++ dataviz/mpl_squares.py
@@ -5,7 +5,7 @@ squares = [1, 4, 9, 16, 25]
 # plt.plot(input_values, squares, linewidth=5)
 
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s=100)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
